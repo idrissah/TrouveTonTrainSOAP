8e6a125f632208751747ae4844dd3cd7668feb30
IDRISSA Hilda <hilda.idrissa@soprasteria.com>
2020-04-18 21:32:56 +0200

    fix maven project

--- src/servlets/HelloServlet.java
+++ src/servlets/HelloServlet.java
@@ -2,11 +2,13 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
+@WebServlet("/")
 public class HelloServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
